15dbad7586019c01d598f61438e3358a2d73be61
Amruta Patil <156510227+adira1999@users.noreply.github.com>
2024-10-28 15:02:58 +0530

    Add files via upload

---
+++ docker-volume/docker-volume/src/main/java/com/example/docker_volume/DockerVolumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_volume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerVolumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerVolumeApplication.class, args);
+	}
+
+}
---
+++ docker-volume/docker-volume/src/test/java/com/example/docker_volume/DockerVolumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_volume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerVolumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
